5523bc15ce7fd43b9e0554fdd8ffb52179a1ac58
Raphael Lima <raphaeldesousalm@gmail.com>
2025-03-19 10:53:40 -0300

    matriz

--- main.cpp
+++ main.cpp
@@ -5,104 +5,161 @@
 
 using namespace std;
 
+
 void calculosMatriz(int tamanho, int qtdThradsAtual){
     /*
-    Inicializa a função para gerar números aleatórios, a time(0) retorna um núemro em segundos desde 1 de jan de 1970 
-    e a srand() gera uma sequência de números aleatórios , como o parâmetro sempre mudará graças ao time(0) a sequência sempre 
-    será diferente em cada execução.
+    Inicializa a função para gerar números aleatórios, a time(0) retorna um número em segundos desde 1 de jan de 1970 
+    e a srand() gera uma sequência de números aleatórios , como o parâmetro sempre mudará devido ao time(0), a sequência 
+    sempre será diferente em cada execução.
     */
-   srand(time(0)); 
+    srand(time(0)); 
 
-   // Aloca matriz de tamanho 10 usando ponteiros
-   int* matriz = new int[tamanho]; 
 
-   // Preenche as matrizes com números aleatórios entre 0 e 255
-   for (int i = 0; i < tamanho; i++) {
-       *(matriz + i) = rand() % 256;
-   }
 
-   // Exibe os valores armazenados na matriz
-   cout << "Matriz original de tamanho " << tamanho << ":" << endl;
-   for (int i = 0; i < tamanho; i++) {
-       cout << *(matriz + i) << " ";
-   }
-   cout << endl;
+    // Aloca matriz de tamanho 10 (nxn) usando ponteiros
+    int *matriz = (int *)malloc(tamanho * tamanho * sizeof(int));
 
-   // Criar uma nova matriz para armazenar os novos valores após a operação.
-   int* nova_matriz = new int[tamanho];
 
-   // Inicia a contagem de tempo
-   double inicio = omp_get_wtime(); 
+
+    // Preenche as matrizes com números aleatórios entre 0 e 255
+    /*for (int i = 0; i < tamanho; i++) {
+        *(matriz + i) = rand() % 256;
+    }*/
+    for (int i = 0; i < tamanho; i++) {
+        for (int j = 0; j < tamanho; j++) {
+            *(matriz + i * tamanho + j) = rand() % 256;  //tamanho = n (número de colunas)
+        }
+    }
+
+
+
+    // Exibe os valores armazenados na matriz
+    cout << "Matriz original de tamanho " << tamanho << ":" << endl;
+
+    for (int i = 0; i < tamanho; i++) {
+        for (int j = 0; j < tamanho; j++) {
+            cout << *(matriz + i * tamanho + j) << " ";  //tamanho = n (número de colunas)
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    // Criar uma nova matriz para armazenar os novos valores após a operação.
+    int *nova_matriz = (int *)malloc(tamanho * tamanho * sizeof(int));
+
+    // Inicia a contagem de tempo
+    double inicio = omp_get_wtime(); 
 
     // inicia a programacao em paralelo
-    omp_set_num_threads(qtdThradsAtual);
+    #pragma omp parallel for num_threads(qtdThradsAtual)
+    {    
+    
 
-    #pragma omp parallel
-{
-    // retorna o número da thread
-    int nThread = omp_get_thread_num(); 
-    // retona a quantidade totais de threads
-    int totalNumThreads = omp_get_num_threads();
-    // Inicio da matriz
-    int inicio = (tamanho / totalNumThreads) * nThread;
-    // Fim da matriz
-    int fim = (nThread == totalNumThreads - 1) ? tamanho : (tamanho / totalNumThreads) * (nThread + 1);
+        // Retorna o número da thread atual
+        int thread_id = omp_get_thread_num();
 
-    /* Substituir os elementos pela média dos vizinhos, se for a primeira posição pega o próprio valor e o vizinho a dirita, 
-   se for a primeira posição pega o próprio valor e o vizinho a esquerda 
-   e se for um elemento que não está nas extremidades pega o vizinho da esquerda e o da direita.
-   */
-  #pragma omp for
-    for (int i = inicio; i < fim; i++) {
-        if (i == 0)
-            *(nova_matriz + i) = (*(matriz + i) + *(matriz + i + 1)) / 2;
-        else if (i == tamanho - 1)
-            *(nova_matriz + i) = (*(matriz + i) + *(matriz + i - 1)) / 2;
-        else
-            *(nova_matriz + i) = (*(matriz + i - 1) + *(matriz + i + 1)) / 2;
+        /* divide a matriz de acordo com o número de threads, cada threads irá processar (tamanho da matriz/ n  de thears) linhas ou colunas já que a matriz é sempre quadrada tanto faz.
+        */
+        int linhas_por_thread = tamanho / qtdThradsAtual;
+
+        // Calcula onde a thread atual começa na matriz
+        int inicio = thread_id * linhas_por_thread;
+
+        // Calcula onde a thread atual termina na matriz
+        int fim = inicio + linhas_por_thread;
+
+        // Percorre matriz
+            for (int i = inicio; i < fim; i++) {
+                for (int j = inicio; j < fim; j++) {
+                    // soma os vizinhos
+                    int soma = 0;
+                    // conta os vizinhos
+                    int count = 0;
+                    
+                    // Percorre os viznhos do elemento, -1 começa "átras dele".
+                    /*
+                        (-1,-1) (-1,0) (-1,1)
+                        (0,-1)  (i,j)   (0,1)
+                        (1,-1)  (1,0)   (1,1)
+                    */
+                    for (int vi = -1; vi <= 1; vi++) {
+                        for (int vj = -1; vj <= 1; vj++) {
+
+                            // calcula os indices dos vizinhos (i e j são coordenadas do elemento central da matriz)
+                            // vi e vj percorre os vizinhos
+                            // ni linha do vizinho e nj a coluna do vizinho
+                            int ni = i + vi;
+                            int nj = j + vj;
+                            /*
+                                - ni >= 0 && ni < fim && nj >= 0 && nj < fim usado em elementos nas bordas evita contar os viznhos que não existem nos elementos das bordas
+                                - !(vi == 0 && vj == 0): Não conta o prório elemento
+                            */
+                            if (ni >= 0 && ni < fim && nj >= 0 && nj < fim && !(vi == 0 && vj == 0)) {
+                                soma += *(matriz + ni * fim + nj);
+                                count++;
+                            }
+                        }
+                    }
+                    *(nova_matriz + i * tamanho + j) = (soma / count);
+                }
+            }
+
     }
-}
 
     // Finaliza a contagem de tempo
-   double fim = omp_get_wtime(); 
+    double fim = omp_get_wtime(); 
 
-   // Copia os valores da nova matriz para a matriz original
-   for (int i = 0; i < tamanho; i++) {
-       *(matriz + i) = *(nova_matriz + i);
-   }
+    // Exibe os valores após a substituição
+    cout << "Matriz apos substituicao:" << endl;
 
-   // Exibe os valores após a substituição
-   cout << "Matriz apos substituicao:" << endl;
-   for (int i = 0; i < tamanho; i++) {
-       cout << *(matriz + i) << " ";
-   }
-   cout << endl;
+    for (int i = 0; i < tamanho; i++) {
+        for (int j = 0; j < tamanho; j++) {
+            cout << *(nova_matriz + i * tamanho + j) << " ";  //tamanho = n (número de colunas)
+        }
+        cout << endl;
+    }
+    cout << endl;
 
-   // Tempo de execução da thread e da matriz
-   cout << "Tempo de execucao com: " << qtdThradsAtual << " threads." << endl;
-   cout << "Tempo de execução para tamanho " << tamanho << ": " << (fim - inicio) << " segundos." << endl;
+    // Tempo de execução da thread e da matriz
+    cout << "Tempo de execucao com: " << qtdThradsAtual << " threads." << endl;
+    cout << "Tempo de execução para tamanho " << tamanho << "x" << tamanho << ": " << (fim - inicio) << " segundos." << endl;
 
-   // Libera a memória alocada
-   delete[] matriz; 
-   delete[] nova_matriz;
+    // Libera a memória alocada
+    delete[] matriz; 
+    delete[] nova_matriz;
+
 }
 
+
 int main() {
+
+    // 1 threads
+    calculosMatriz(10, 1); 
+    calculosMatriz(50, 1); 
+    calculosMatriz(100, 1);
+
 
     // 2 threads
     calculosMatriz(10, 2); 
     calculosMatriz(50, 2); 
     calculosMatriz(100, 2);
 
+
     // 4 threads
     calculosMatriz(10, 4); 
     calculosMatriz(50, 4); 
     calculosMatriz(100, 4);
+
 
     // 8 threads
     calculosMatriz(10, 8); 
     calculosMatriz(50, 8); 
     calculosMatriz(100, 8);
 
+
     return 0;
+
 }
+
+
+
